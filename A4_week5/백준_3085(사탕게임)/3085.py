0cad545b6e01e11b34a8c870b3535910a521d949
이효원 <129304827+ehyowon@users.noreply.github.com>
2024-05-22 12:22:54 +0900

    Update 3085.py

--- A4_week5/백준_3085(사탕게임)/3085.py
+++ A4_week5/백준_3085(사탕게임)/3085.py
@@ -1,1 +1,28 @@
+n = int(input())
 
+candy = [list(input()) for _ in range(n)]
+a = candy[0][0]
+row, col = [], []
+
+for i in range(n):
+	cnt = 0
+	for j in range(n):
+		if candy[i][j] == a:
+			cnt += 1
+		a = candy[i][j]
+	row.append(cnt)
+
+for i in range(n):
+	cnt = 0
+	for j in range(n):
+		if candy[j][i] == a:
+			cnt += 1
+		a = candy[j][i]
+	col.append(cnt)
+
+row_max = max(row)
+col_max = max(col)
+if row_max > col_max:
+	print(row_max)
+else:
+	print(col_max)
